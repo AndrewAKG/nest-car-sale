949fdf88da8ff09f322ad952720c74ea9ef91c76
AndrewAKG <andrewakram1997@gmail.com>
2023-04-03 22:49:41 +0200

    add e2e testing and applying app configuration through @nestjs/config

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,8 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const cookeSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookeSession({
-      keys: ['abcdabcd'],
-    }),
-  );
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
 bootstrap();
